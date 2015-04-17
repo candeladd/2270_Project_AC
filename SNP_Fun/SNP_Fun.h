c7dec6fb7652afb632bff784f958644bbbf20f83
austinholler <auho1670@colorado.edu>
2015-04-17 14:58:30 -0600

    Project Files

--- SNP_Fun/SNP_Fun.h
+++ SNP_Fun/SNP_Fun.h
@@ -1,6 +1,15 @@
 #ifndef SNP_FUN_H
 #define SNP_FUN_H
+#include <vector>
+#include <iostream>
 
+const int gene = 10;
+const int chrom = 23;
+
+struct rsid
+{
+    std::string info;
+};
 
 class SNP_Fun
 {
@@ -9,6 +18,8 @@ class SNP_Fun
         ~SNP_Fun();
     protected:
     private:
+        std::vector<rsid> foo [gene][chrom];
+
 };
 
 #endif // SNP_FUN_H
